dcd266133a3cb621f3b4f7398be29ba6b4e951ca
zheirus <zheioSDLG.500@hotmail.com>
2022-02-01 21:34:31 -0500

    Actualizacion 2.2
    mejoras en la presentacion y la implementacion de la navegacion

--- JavaScript/interaccion.js
+++ JavaScript/interaccion.js
@@ -1,3 +1,19 @@
 const fullpageV1 = new fullpage("#fullpage", {
+    autoScrolling: true, // Se activa el scroll.
+    fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
+    fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
+    easing: 'easeInOutCubic', // Funcion de tiempo de la animacion.
+    scrollingSpeed: 800, // Velocidad del scroll. Valores: en milisegundos.
+    css3: true, // Si usara CSS3 o javascript.
+    easingcss3: 'ease-out', // Curva de velocidad del efecto.
+    loopBottom: true, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
+
+    navigation: true, // Muesta la barra de navegación.
+    menu: '#menu', // Menu de navegación.
+    anchors: ['inicio', 'Mercurio-Venus', 'Venus-Tierra', 'Tierra-Luna', 'Tierra-Marte', 'Marte-Jupiter', 'Jupiter-Saturno', 'Saturno-Urano', 'Urano-Neptuno', 'contactos'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+    navigationTooltips: ['inicio', 'Mercurio-Venus', 'Venus-Tierra', 'Tierra-Luna', 'Tierra-Marte', 'Marte-Jupiter', 'Jupiter-Saturno', 'Saturno-Urano', 'Urano-Neptuno', 'contactos'], // Tooltips que mostrara por cada boton.
+    showActiveTooltip: false, // Mostrar tooltip activa.
+
+
 
 });
